bca45b878a057adc0d2a90b559aa467c6c3e5a7c
joacoxmdb <joaquinpapagianacopoulos@gmail.com>
2025-06-08 14:59:08 -0300

    Fix production server with Gunicorn

--- main.py
+++ main.py
@@ -58,5 +58,8 @@ def api_generate_report():
         }), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
+
+# Para Gunicorn
+app.debug = False
